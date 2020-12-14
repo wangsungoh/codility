55066241c9d47afefa35e200451bfdac9ffbaf01
Wangsung Oh <wangsungoh@gmail.com>
2020-12-14 22:34:15 +0900

    remove log

--- 01.binary_gap.js
+++ 01.binary_gap.js
@@ -2,8 +2,7 @@ function solution(N) {
   let result = 0;
   let max = 0;
   const binaryArray = N.toString(2);
-  console.log(binaryArray);
-
+  
   for(let i = 0; i <  binaryArray.length; i++) {
     if (binaryArray[i] === '0') {
       result += 1;
@@ -12,8 +11,6 @@ function solution(N) {
       result = 0;
     }
   }
-
-  console.log(max)
   
   return max;
 }
